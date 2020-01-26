89709a8f2b19dc52e7fe8428dc449d5330f80d35
tuhin <tuhinh02@gmail.com>
2020-01-26 12:47:54 +0600

    chat room application

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 # Use sqlite3 as the database for Active Record
 gem 'pg'
 gem 'devise'
+gem 'redis', '~> 3.2'
 gem 'bootstrap', '~> 4.0.0.alpha3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+  get 'chat_rooms/new'
   devise_for :users
+
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index'
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
